59d35b1ac1b273188595f2100137a13117c520cc
Gil <ggonzalere@gmail.com>
2017-05-01 17:06:07 -0500

    Empezando de cero

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501202157) do
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string "Nombre"
-    t.string "Email"
-    t.string "Contraseña"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
