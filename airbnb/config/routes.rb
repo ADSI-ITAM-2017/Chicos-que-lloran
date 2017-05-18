be169dedabf57e2424bea7f7020cae3d2bb26300
Gil <ggonzalere@gmail.com>
2017-05-18 12:13:36 -0500

    Transferencia a comentarios de propiedad

--- airbnb/config/routes.rb
+++ airbnb/config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get 'registro', to: 'usuarios#new', as: 'registro'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'comentario', to: 'p_comments#new', as: 'comentario'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
