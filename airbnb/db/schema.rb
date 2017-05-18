11ce67d152f817374e9341d2ccb48c72ff938c96
Gil <ggonzalere@gmail.com>
2017-05-18 09:24:47 -0500

    Lectura de mensajes por usuario

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518100038) do
+ActiveRecord::Schema.define(version: 20170518141529) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "usuario1_id"
+    t.integer "usuario2_id"
+  end
 
   create_table "propiedads", force: :cascade do |t|
     t.string "arrendador"
