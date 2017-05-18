768ff9c7fbd4a61bab05f35e772fde4f1c4752eb
Rodrigo Ferrer Bisteni <rodrigoferrerb@gmail.com>
2017-05-18 07:40:18 -0500

    Habia puesto los cambios a controlador y vistas en otro repositorio local xdxdxd. Ahora si funciona la busqueda

--- airbnb/app/models/propiedad.rb
+++ airbnb/app/models/propiedad.rb
@@ -1,2 +1,5 @@
 class Propiedad < ApplicationRecord
+	def self.search(search)
+  			where("arrendador LIKE ? OR direccion LIKE ? OR descripcion LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+	end
 end
